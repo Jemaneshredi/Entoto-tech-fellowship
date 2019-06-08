69d9034dc2eb0da5fdc905463ac1f194734d7ea6
Jemaneshredi <jemaninegash@gmail.com>
2019-06-08 03:36:04 -0700

    my second js file

---
+++ week1/day4.js
@@ -0,0 +1,35 @@
+
+//  var weatherTemperature = 15
+// console.log(weatherTemperature)
+// if ( weatherTemperature < 10 )
+// {
+//   console.log("it's cold outside")
+// }
+// else if ( weatherTemperature > 20 ){
+//   console.log("it's warm outside")
+// }
+// else {
+//   console.log("it's nice outside")
+// }
+
+  var book = {
+ title: "sememen",
+ author: "jema",
+ releaseDate: 1980,  }
+ 
+console.log(book)  
+console.log(book.title) 
+
+var food = ["vegetable", "shiro", "bread"]
+console.log(food)
+var i;
+
+for (i = 0; i<3; i++){
+  console.log (food[i])
+ }
+      var count = 1;
+    while (count <= 10 ){
+      console.log(count);
+          count += 1;
+      
+    }
