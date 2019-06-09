ff826fe992ecd2a7401f8a92da0b662c13646295
Jemaneshredi <jemaninegash@gmail.com>
2019-06-09 05:49:54 -0700

    the second file

--- week1/day4.js
+++ week1/day4.js
@@ -1,16 +1,4 @@
 
-//  var weatherTemperature = 15
-// console.log(weatherTemperature)
-// if ( weatherTemperature < 10 )
-// {
-//   console.log("it's cold outside")
-// }
-// else if ( weatherTemperature > 20 ){
-//   console.log("it's warm outside")
-// }
-// else {
-//   console.log("it's nice outside")
-// }
 
   var book = {
  title: "sememen",
