1d339941a2d28c34488b7f6459f4d343a44449dd
Jemaneshredi <jemaninegash@gmail.com>
2019-06-18 05:45:58 -0700

    the frist weeek assigment

---
+++ week1/day4.js
@@ -0,0 +1,23 @@
+
+
+  var book = {
+ title: "sememen",
+ author: "jema",
+ releaseDate: 1980,  }
+ 
+console.log(book)  
+console.log(book.title) 
+
+var food = ["vegetable", "shiro", "bread"]
+console.log(food)
+var i;
+
+for (i = 0; i<3; i++){
+  console.log (food[i])
+ }
+      var count = 1;
+    while (count <= 10 ){
+      console.log(count);
+          count += 1;
+      
+    }
